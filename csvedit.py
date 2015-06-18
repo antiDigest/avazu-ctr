000220cb5b2914e04221ce97147f66d4593dd279
Antriksh <antriksh5235@gmail.com>
2015-06-18 22:37:41 +0530

    finding logloss

--- csvedit.py
+++ csvedit.py
@@ -5,17 +5,17 @@ lst = []
 l = []
 
 #Read from the main train.csv
-with open('train.csv','rb') as f:
+with open('train/train.csv','rb') as f:
     rdr = csv.reader(f)
     i = 0
     for row in rdr:
             #l = row[0], row[1], row[2], row[4], row[5], row[6], row[7]
         lst.append(row)
         i += 1
-        if i > 100000:
+        if i > 200000:
             break
 
 #write to a new file subtrain.csv
-with open('subtrain.csv','w') as f:
+with open('train/subtrain.csv','w') as f:
     writer = csv.writer(f)
     writer.writerows(lst)
--- logistic.py
+++ logistic.py
@@ -5,11 +5,19 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, date, time
 from sklearn.linear_model import SGDClassifier
-#from sklearn.linear_model import LinearRegression
 from sklearn.feature_extraction import FeatureHasher
 from sklearn.preprocessing import LabelEncoder
+import scipy as sp
 
 cols = ["C1","banner_pos","site_category", "device_type","device_conn_type","C14","C15","C16","C17","C18","C19","C20","C21", "hour"]
+
+def llfun(act, pred):
+    epsilon = 1e-15
+    pred = sp.maximum(epsilon, pred)
+    pred = sp.minimum(1-epsilon, pred)
+    ll = sum(act*sp.log(pred) + sp.subtract(1,act)*sp.log(sp.subtract(1,pred)))
+    ll = ll * -1.0/len(act)
+    return ll
 
 # add two columns for hour and weekday
 def dayhour(timestr):
@@ -20,7 +28,7 @@ fh = FeatureHasher(n_features = 2**20, input_type="string")
 
 # Train classifier
 clf = SGDClassifier(loss="log", n_iter=1)
-train = pd.read_csv("subtrain.csv", chunksize = 100000, iterator = True)
+train = pd.read_csv("train/subtrain.csv", chunksize = 100000, iterator = True, skiprows=100000)
 all_classes = np.array([0, 1])
 for chunk in train:
     y_train = chunk["click"]
@@ -32,14 +40,20 @@ for chunk in train:
     
 # Create a submission file
 usecols = cols + ["id"]
-X_test = pd.read_csv("subtest.csv", usecols=usecols)
+X_test = pd.read_csv("test/mtest.csv", usecols=usecols)
 X_test = X_test.join(pd.DataFrame([dayhour(x) for x in X_test.hour], columns=["wd", "hr"]))
 X_test.drop(["hour"], axis=1, inplace = True)
 
 X_enc_test = fh.transform(np.asarray(X_test.astype(str)))
 
+y_act = pd.read_csv("test/mtest.csv", usecols=['click'])
+print y_act
 y_pred = clf.predict_proba(X_enc_test)[:, 1]
-with open("submission_sgd.csv", "w") as f:
+print y_pred
+with open('logloss.txt','w') as f:
+    f.write(llfun(y_act, y_pred))
+
+with open("submission/submission_sgd.csv", "w") as f:
     f.write("id,click\n")
     for idx, xid in enumerate(X_test.id):
         f.write(str(xid) + "," + "{0:.10f}".format(y_pred[idx]) + "\n")
