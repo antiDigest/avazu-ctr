ca817b016bf519669f204b1f41f4083b143c1960
evamy <antriksh.don@gmail.com>
2017-03-18 06:40:53 -0500

    First Kaggle submission

--- logistic.py
+++ logistic.py
@@ -11,51 +11,66 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import log_loss
 import scipy as sp
 
-cols = ["C1","banner_pos","site_category","app_category", "device_type","device_conn_type","C14","C15","C16","C17","C18","C19","C20","C21", "hour"]
+cols = ["C1", "banner_pos", "site_category", "app_category", "device_type",
+        "device_conn_type", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21", "hour"]
+
 
 def llfun(act, pred):
     epsilon = 1e-15
     pred = sp.maximum(epsilon, pred)
-    pred = sp.minimum(1-epsilon, pred)
-    ll = sum(act*sp.log(pred) + sp.subtract(1,act)*sp.log(sp.subtract(1,pred)))
-    ll = ll * -1.0/len(act)
+    pred = sp.minimum(1 - epsilon, pred)
+    ll = sum(act * sp.log(pred) + sp.subtract(1, act)
+             * sp.log(sp.subtract(1, pred)))
+    ll = ll * -1.0 / len(act)
     return ll
 
 # add two columns for hour and weekday
+
+
 def dayhour(timestr):
     d = datetime.strptime(str(x), "%y%m%d%H")
     return [float(d.weekday()), float(d.hour)]
 
-fh = FeatureHasher(n_features = 2**20, input_type="string")
+fh = FeatureHasher(n_features=2**20, input_type="string")
 
 # Train classifier
 clf = LogisticRegression()
-train = pd.read_csv("testtrain.csv", chunksize = 500000, iterator = True)
+train = pd.read_csv("train", chunksize=500000, iterator=True)
 all_classes = np.array([0, 1])
+i = 0
 for chunk in train:
+    print "training chunk", i
+    i += 1
     y_train = chunk["click"]
     chunk = chunk[cols]
-    chunk = chunk.join(pd.DataFrame([dayhour(x) for x in chunk.hour], columns=["wd", "hr"]))
-    chunk.drop(["hour"], axis=1, inplace = True)
+    chunk = chunk.join(pd.DataFrame([dayhour(x)
+                                     for x in chunk.hour], columns=["wd", "hr"]))
+    chunk.drop(["hour"], axis=1, inplace=True)
     Xcat = fh.transform(np.asarray(chunk.astype(str)))
     clf.fit(Xcat, y_train)
-    
+
+
+print "Trained all, proceeding to submission."
 # Create a submission file
-usecols = cols + ["id"]
-X_test = pd.read_csv("testtest.csv", usecols=usecols)
-X_test = X_test.join(pd.DataFrame([dayhour(x) for x in X_test.hour], columns=["wd", "hr"]))
-X_test.drop(["hour"], axis=1, inplace = True)
+testcols = ["id"] + cols
+X_test = pd.read_csv("test", usecols=testcols)
+
+X_test = X_test.join(pd.DataFrame([dayhour(x)
+                                   for x in X_test.hour], columns=["wd", "hr"]))
+X_test.drop(["hour"], axis=1, inplace=True)
 
 X_enc_test = fh.transform(np.asarray(X_test.astype(str)))
 
-y_act = pd.read_csv("testtest.csv", usecols=['click'])
+# y_act = pd.read_csv("test", usecols=['click'])
 y_pred = clf.predict_proba(X_enc_test)[:, 1]
 
-with open('logloss_logistic.txt','a') as f:
-    f.write('\n'+str(log_loss(y_act, y_pred)))
+# with open('logloss_logistic.txt', 'a') as f:
+#     f.write('\n' + str(log_loss(y_act, y_pred)))
 
-with open("submission_logistic.csv", "w") as f:
+with open("submission.csv", "w") as f:
     f.write("id,click\n")
     for idx, xid in enumerate(X_test.id):
         f.write(str(xid) + "," + "{0:.10f}".format(y_pred[idx]) + "\n")
 f.close()
+
+print "Submitted"
